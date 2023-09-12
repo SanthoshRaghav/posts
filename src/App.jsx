4ae8f97d1771659ee9eedd4409e45a9acc6aa40f
Santhosh Raghav <santhoshraghav@Santhoshs-Mac-mini.local>
2023-09-12 11:42:17 +0530

    counter application is done with redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
-  return <>Posts</>;
+  return (
+    <>
+      <Counter />
+    </>
+  );
 }
 
 export default App;
